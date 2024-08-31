2b7f6a8d35fb3edfddcf03cdcdc62976f4d16559
Jayesh170 <202202367@vupune.ac.in>
2024-08-31 19:26:16 +0530

    task-bar updated

--- App.tsx
+++ App.tsx
@@ -3,12 +3,26 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View>
-      <Text>ClassRoom</Text>
+    <View style={[styles.headerBar]}>
+      <Text style={[styles.textCenter]}>ClassRoom</Text>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headerBar : {
+    height : 50,
+    width: '100%',
+    backgroundColor: '#95a832',
+    borderRadius : 4,
+    justifyContent: 'center',
+    alignItems : 'center'
+  },
+  textCenter : {
+    fontSize : 24,
+    fontFamily : 'bold',
+    paddingHorizontal : 5
+  }
+})
