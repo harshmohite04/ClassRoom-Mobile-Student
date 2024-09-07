a90916b4983fdbb9e4e767a2cc59f12444b299ae
Jayesh170 <202202367@vupune.ac.in>
2024-09-07 23:55:25 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -8,18 +8,24 @@ import First from './components/Page/First'
 import Second from './components/Page/Second';
 import Phone from './components/Page/Phone';
 import Third from './components/Page/Third';
-
+import NotiFy from './components/Page/NoNotiFy';
+import NoNet from './components/Page/NoNetwork';
+import NoVideo from './components/Page/NoVideos'
 const Stack = createStackNavigator();
 
  function App() {
 
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Phone" screenOptions={{headerShown:false}}> 
+    <Stack.Navigator initialRouteName="NoVideo" screenOptions={{headerShown:false}}> 
       <Stack.Screen name ="First" component={First} />
       <Stack.Screen name ="Second" component={Second} />
       <Stack.Screen name ="Third" component={Third} />
       <Stack.Screen name ="Phone" component={Phone} />
+      <Stack.Screen name ="Notify" component={NotiFy} />
+      <Stack.Screen name ="NoNet" component={NoNet} />
+      <Stack.Screen name ="NoVideo" component={NoVideo} />
+
     </Stack.Navigator>
     </NavigationContainer>
 
---
+++ components/Page/NoVideos.tsx
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View, Dimensions, useColorScheme, TouchableOpacity } from 'react-native';
+import React from 'react';
+import ImageLight from '../../assets/svg/NoVideos';
+import ImageDark from '../../assets/svg/NoVideos(dark)';
+
+const { width } = Dimensions.get('window');
+const scale = width / 320;
+
+const lightTheme = {
+  background: '#FFFFFF',
+  textPrimary: '#1F1F39',
+  textSecondary: '#858597',
+  buttonBackground: '#3D5CFF',
+  buttonText: '#FFFFFF',
+};
+
+const darkTheme = {
+  background: '#1F1F39',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#A9A9A9',
+  buttonBackground: '#3D5CFF',
+  buttonText: '#FFFFFF',
+};
+
+export default function NoVideos() {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? darkTheme : lightTheme;
+  const ImageComponent = colorScheme === 'dark' ? ImageDark : ImageLight;
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <View style={styles.ImageCard}>
+        <ImageComponent />
+      </View>
+      <View>
+        <Text style={[styles.TextStyle1, { color: theme.textPrimary }]}>
+        No videos!
+        </Text>
+        <Text style={[styles.TextStyle2, { color: theme.textSecondary }]}>
+        Here is no video you want at the 
+        moment.
+        </Text>
+      </View>
+
+      
+      <TouchableOpacity style={[styles.button, { backgroundColor: theme.buttonBackground }]}>
+        <Text style={[styles.buttonText, { color: theme.buttonText }]}>
+          Search More
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 200 * scale,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ImageCard: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ImageStyle: {
+    height: 150 * scale, // Adjust the size as per your requirement
+    width: 150 * scale,
+    alignSelf: 'center',
+  },
+  TextCard: {
+    textAlign: 'center',
+  },
+  TextStyle1: {
+    textAlign: 'center',
+    fontSize: 17 * scale,
+    fontWeight: 'bold',
+  },
+  TextStyle2: {
+    textAlign: 'center',
+    fontSize: 12 * scale,
+    fontWeight: 'bold',
+    marginTop: 10 * scale,
+  },
+  button: {
+    marginTop: 20 * scale, 
+    paddingVertical: 12 * scale, 
+    paddingHorizontal: 50 * scale, 
+    borderRadius: 12 * scale,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16 * scale, 
+    fontWeight: 'bold',
+  },
+});
